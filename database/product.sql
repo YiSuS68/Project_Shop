59b199555502bc267ddfed02c2556717285f5cd0
YiSuS68 <nhhungcntt@gmail.com>
2022-01-01 18:55:31 +0700

    db.+bill+dtbill

--- database/product.sql
+++ database/product.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 26, 2021 at 06:59 AM
+-- Generation Time: Jan 01, 2022 at 11:52 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -24,6 +24,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_serve` int(11) NOT NULL,
+  `date_order` date NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `price_bill` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` enum('Nam','Nữ','Khác') NOT NULL,
+  `gmail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_bill`
+--
+
+CREATE TABLE `detail_bill` (
+  `id` int(11) NOT NULL,
+  `id_bill` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `quantity` int(5) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `producer`
+--
+
+CREATE TABLE `producer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -38,9 +98,57 @@ CREATE TABLE `product` (
   `id_producer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `serve`
+--
+
+CREATE TABLE `serve` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` enum('Nam','Nữ','Khác') NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `identity` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `level` int(2) NOT NULL,
+  `wage` float NOT NULL,
+  `account` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_serve` (`id_serve`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `detail_bill`
+--
+ALTER TABLE `detail_bill`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_bill` (`id_bill`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_product` (`id_product`);
+
+--
+-- Indexes for table `producer`
+--
+ALTER TABLE `producer`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product`
@@ -50,18 +158,69 @@ ALTER TABLE `product`
   ADD KEY `id_producer` (`id_producer`);
 
 --
+-- Indexes for table `serve`
+--
+ALTER TABLE `serve`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_bill`
+--
+ALTER TABLE `detail_bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `producer`
+--
+ALTER TABLE `producer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `serve`
+--
+ALTER TABLE `serve`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`),
+  ADD CONSTRAINT `bill_ibfk_2` FOREIGN KEY (`id_serve`) REFERENCES `serve` (`id`);
+
+--
+-- Constraints for table `detail_bill`
+--
+ALTER TABLE `detail_bill`
+  ADD CONSTRAINT `detail_bill_ibfk_1` FOREIGN KEY (`id_bill`) REFERENCES `bill` (`id`),
+  ADD CONSTRAINT `detail_bill_ibfk_2` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`),
+  ADD CONSTRAINT `detail_bill_ibfk_3` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`);
 
 --
 -- Constraints for table `product`

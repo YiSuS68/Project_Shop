91ef54620de3ffd1eba624ec073edf9a31b9f795
Bùi Việt Anh <loverbee01@gmail.com>
2022-02-08 20:05:08 +0700

    08/02/2022
    08/02/2022

--- login_register.php
+++ login_register.php
@@ -14,16 +14,50 @@
 <body>
 
     <div>
-        <!-- navbar -->
-        <div>
-            <?php include "component_navbar_shortcut.php" ?>
-        </div>
 
-        <!-- lấy lỗi hiển thị ra -->
+        <!-- navbar -->
+        <div class="position-rel space-flex-wrap p_t-b items-center setup992 bg-white" id="header-navbar">   
+            <div class="items-center space-flex container flex-inhe">
+
+                <!-- part one -->
+                <a class="m_r2 m_b items-center n-decoration" id="logo" href="index.php">Green Shop</a>
+
+                    <!-- nút bấm khi màn hình thu nhỏ dưới 992px -->
+                    <button class="buttonMini" onclick="myButtonMini()"><i class="fas fa-bars" id="button_hidden_small"></i></button>
+
+                <!-- part two -->
+                <div class="header-setup all-center w100" id="collapse_btn">
+
+                    <!-- 2.1 -->
+                    <div class="flex-auto">
+                        <ul class="header-click space-flex-wrap">
+                            <li><a class="hover dark n-decoration p d-block" href="index.php">Home</a></li>
+                            <li><a class="hover dark n-decoration p d-block" href="about.php">About</a></li>
+                            <li><a class="hover dark n-decoration p d-block" href="shop.php">Shop</a></li>
+                            <li><a class="hover dark n-decoration p d-block" href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- 2.2 -->
+                    <div class="header-all p_t-b space-flex-wrap position-rel items-center text-center">
+                        <!-- tạo ra popup của tìm kiếm ở đây -->
+                        <div class="header-search m_t2 m_b4">
+                            <div class="header-input">
+                                <input type="text" class="search-time black" id="inputMobileSearch" placeholder="Search...">
+                                <div class="search-icon black">
+                                    <i class="search w20 fas fa-search"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
         
         <!-- đăng nhập và đăng ký -->
         <div class="center-form">
-
             <!-- part one thanh tab của đăng nhập đăng ký -->
             <div class="tabs">
                 <div class="tab-item active">
@@ -38,7 +72,6 @@
                 <div class="line"></div>
             </div>
 
-
             <!-- part two nội dung form của thanh đăng nhập, đăng ký -->
             <div>
                 <?php
@@ -49,11 +82,9 @@
 
                 <!-- 2.2 đăng ký -->
                 <?php include "signup.php" ?>
-
             </div>
-
-
         </div>
+
     </div>
 
 

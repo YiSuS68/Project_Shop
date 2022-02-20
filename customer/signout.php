ad3900e5f0aeb1150e109ab8132d0f6a07445b3a
YiSuS68 <nhhungcntt@gmail.com>
2022-02-20 23:17:26 +0700

    .

--- customer/signout.php
+++ customer/signout.php
@@ -4,8 +4,12 @@
 session_start();
 
 // đăng xuất, xóa đi id và tên sau
+// print_r($_SESSION['id']);
+// die($_SESSION['user_name']);
 unset($_SESSION['id']);
 unset($_SESSION['user_name']);
+// print_r($_SESSION['id']);
+// die($_SESSION['user_name']);
 setcookie('remember',null,-1);
-
+// die($_COOKIE['id']);
 header('location:../index.php');
